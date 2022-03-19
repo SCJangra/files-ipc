00caafc1efcfb474d5fc67b0a559a3cadca503e5
Sachin Charakhwal <sachincharakhwal@gmail.com>
2022-03-19 13:56:12 +0530

    feat(rpc): add methods `delete_file` and `delete_dir`
    The method `delete_dir` only deletes empty directories

--- src/rpc.rs
+++ src/rpc.rs
@@ -21,6 +21,12 @@ pub trait Rpc {
 
     #[rpc(name = "create_dir")]
     fn create_dir(&self, name: String, dir: FileId) -> JrpcFutResult<FileId>;
+
+    #[rpc(name = "delete_file")]
+    fn delete_file(&self, id: FileId) -> JrpcFutResult<bool>;
+
+    #[rpc(name = "delete_dir")]
+    fn delete_dir(&self, id: FileId) -> JrpcFutResult<bool>;
 }
 
 pub struct RpcImpl {}
@@ -51,6 +57,20 @@ impl Rpc for RpcImpl {
         Box::pin(async move {
             let id = lib::create_dir(&name, &dir).map_err(to_rpc_err).await?;
             Ok(id)
+        })
+    }
+
+    fn delete_file(&self, id: FileId) -> JrpcFutResult<bool> {
+        Box::pin(async move {
+            let res = lib::delete_file(&id).map_err(to_rpc_err).await?;
+            Ok(res)
+        })
+    }
+
+    fn delete_dir(&self, id: FileId) -> JrpcFutResult<bool> {
+        Box::pin(async move {
+            let res = lib::delete_dir(&id).map_err(to_rpc_err).await?;
+            Ok(res)
         })
     }
 }
